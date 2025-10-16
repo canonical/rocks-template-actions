d2672ec40f2d21624c1818d1ab8bcc6cafb9ed7d
Zhijie Yang <zhijie.yang@canonical.com>
2025-10-16 17:35:59 +0200

    feat: add support for user-defined registries

--- actions/read-ci-config/src/process_ci_config.py
+++ actions/read-ci-config/src/process_ci_config.py
@@ -3,10 +3,14 @@ import glob
 import json
 import os
 import re
+from collections import defaultdict
+from typing import Optional, Union
 
 import pydantic
 import yaml
 from pydantic import BaseModel, Field
+
+from .auth import AUTH_MODELS, AuthType
 
 
 class GHCRConfig(BaseModel):
@@ -26,10 +30,79 @@ class GHCRConfig(BaseModel):
         return v
 
 
+class RegistrySecretEntry(BaseModel):
+    # Field `auth` is an enumerate of different authentication methods
+    method: str = Field(
+        ..., description="The type of authentication method", alias="method"
+    )
+    config: Union[tuple(AUTH_MODELS.values())] = Field(
+        ..., description="The authentication method for the registry"
+    )
+
+    model_config = pydantic.ConfigDict(extra="forbid")
+
+    @pydantic.model_serializer
+    def add_prefix(self):
+        r = {"registry-auth-method": self.method}
+        r.update(self.config.model_dump())  # pylint: disable=no-member
+        return r
+
+    @pydantic.field_validator("method", mode="before")
+    def _ensure_method_known(cls, v):  # pylint: disable=no-self-argument
+        if isinstance(v, str):
+            try:
+                return AuthType(v)
+            except ValueError as e:
+                raise ValueError(
+                    f"Invalid auth method '{v}'. Supported methods are: {', '.join([m.value for m in AuthType])}."
+                ) from e
+        return v
+
+    @pydantic.field_validator("config", mode="before")
+    def _ensure_config_type(cls, v, info):  # pylint: disable=no-self-argument
+        if v is None:
+            raise ValueError("Auth config must be provided.")
+        if isinstance(v, AUTH_MODELS[AuthType.BASIC]):
+            return v
+        if not isinstance(v, dict):
+            raise ValueError("Auth config must be a dictionary.")
+        method = info.data.get("method")
+        model_cls = AUTH_MODELS.get(method)
+        if model_cls is None:
+            raise ValueError(
+                f"Unsupported auth method '{method}'. Supported methods are: {', '.join([m.value for m in AuthType])}."
+            )
+        return model_cls(**v)
+
+
+class RegistryConfigEntry(BaseModel):
+    uri: str = Field(..., description="The URL of the registry")
+    auth: RegistrySecretEntry = Field(
+        ...,
+        description="The GitHub Action secrets for authentication",
+    )
+
+    model_config = pydantic.ConfigDict(extra="forbid")
+
+    @pydantic.field_validator("auth", mode="before")
+    def _unpack_auth_list(cls, v):  # pylint: disable=no-self-argument
+        if not isinstance(v, (list)):
+            raise ValueError("Auth must be provided as a list.")
+        if len(v) != 1:
+            raise ValueError("Auth list must contain exactly one entry.")
+        return v[0]
+
+
 class ImageEntry(BaseModel):
     directory: str = Field(
         ..., description="Path to the directory containing the rockcraft.yaml"
     )
+
+    registries: Optional[list[str]] = Field(
+        description="List of registry names that match the registry config",
+        default_factory=list,
+    )
+    # TODO: Add support for building pro-enabled rocks
 
     model_config = pydantic.ConfigDict(extra="forbid")
 
@@ -38,6 +111,10 @@ class CIConfig(BaseModel):
     version: int = Field(..., description="Version of the CI configuration")
     ghcr: GHCRConfig = Field(
         ..., description="Configuration for GitHub Container Registry"
+    )
+    # We need to construct registries before images to validate the references of "registries" in images
+    registries: dict[str, RegistryConfigEntry] = Field(
+        description="Mapping of registry names to their configurations"
     )
     images: list[ImageEntry] = Field(description="List of images to be processed")
 
@@ -49,10 +126,31 @@ class CIConfig(BaseModel):
             raise ValueError("Only version 1 of the CI configuration is supported.")
         return v
 
+    @pydantic.field_validator("registries", "images", mode="before")
+    def _ensure_registries_dict(cls, v):  # pylint: disable=no-self-argument
+        if v is None:
+            return {}
+        return v
+
     @pydantic.field_validator("images", mode="before")
     def _ensure_images_list(cls, v):  # pylint: disable=no-self-argument
         if v is None:
             return []
+        return v
+
+    @pydantic.field_validator("images", mode="after")
+    def _ensure_image_registries_exist(
+        cls, v, info
+    ):  # pylint: disable=no-self-argument
+        if not v:
+            return []
+        registry_keys = info.data.get("registries", {}).keys()
+        for image in v:
+            for registry in image.registries:
+                if registry not in registry_keys:
+                    raise ValueError(
+                        f"Registry '{registry}' in image '{image.directory}' is not defined in registries."
+                    )
         return v
 
     @pydantic.field_validator("images", mode="after")
@@ -69,7 +167,11 @@ class CIConfig(BaseModel):
                 # Expand the wildcard using glob
                 dirs = glob.glob("**/rockcraft.yaml", recursive=True)
                 for d in dirs:
-                    expanded_images.append(ImageEntry(directory=os.path.dirname(d)))
+                    expanded_images.append(
+                        ImageEntry(
+                            directory=os.path.dirname(d), registries=image.registries
+                        )
+                    )
             else:
                 expanded_images.append(image)
         return expanded_images
@@ -79,7 +181,7 @@ class CIConfig(BaseModel):
         return dir.replace("/", "-")
 
     @staticmethod
-    def image_name_and_tag(image_directory: str) -> tuple[str, str]:
+    def image_name_and_tag(repo_root: str, image_directory: str) -> tuple[str, str]:
         """Read the rockcraft.yaml in the given directory to get the image name and tag.
 
         Args:
@@ -94,7 +196,9 @@ class CIConfig(BaseModel):
         # Pattern to match base version id like '22.04', '20.04', or 'devel'
         base_version_id_pattern = r"(\d{2}(\.|@)\d{2}|devel)$"
         with open(
-            os.path.join(image_directory, "rockcraft.yaml"), "r", encoding="utf-8"
+            os.path.join(repo_root, image_directory, "rockcraft.yaml"),
+            "r",
+            encoding="utf-8",
         ) as f:
             rockcraft = yaml.safe_load(f)
             name = rockcraft["name"]
@@ -117,7 +221,7 @@ class CIConfig(BaseModel):
             tag = f"{version}-{base}_{channel}"
         return name, tag
 
-    def build_matrix(self) -> dict:
+    def build_matrix(self, repo_root: str = "") -> dict:
         """Generate the build matrix for GitHub Actions.
 
         Returns:
@@ -129,7 +233,7 @@ class CIConfig(BaseModel):
             if image.directory in added_image_dirs:
                 continue
             added_image_dirs.add(image.directory)
-            name, tag = self.image_name_and_tag(image.directory)
+            name, tag = self.image_name_and_tag(repo_root, image.directory)
             matrix["include"].append(
                 {
                     "name": name,
@@ -138,6 +242,35 @@ class CIConfig(BaseModel):
                     "artifact-name": self.artifact_name(image.directory),
                 }
             )
+        return matrix
+
+    def upload_matrix(self, repo_root: str = "") -> dict:
+        """Generate the upload matrix for GitHub Actions.
+
+        Returns:
+            dict: Upload matrix
+        """
+        matrix = {"include": []}
+        image_registries = defaultdict(set)
+        for image in self.images:  # pylint: disable=not-an-iterable
+            image_registries[image.directory].update(set(image.registries))
+        for image_directory, registries in image_registries.items():
+            if not registries:
+                continue
+            name, tag = self.image_name_and_tag(repo_root, image_directory)
+            for r in sorted(list(registries)):
+                registry = self.registries[r]  # pylint: disable=unsubscriptable-object
+                matrix["include"].append(
+                    {
+                        "name": name,
+                        "tag": tag,
+                        "artifact-name": self.artifact_name(image_directory),
+                        "registry-uri": registry.uri,
+                        **registry.auth.model_dump(
+                            by_alias=True
+                        ),  # pylint: disable=no-member
+                    }
+                )
         return matrix
 
     def ghcr_config_json(self, **kwargs):
@@ -152,6 +285,9 @@ def main():
     )
     parser.add_argument(
         "config_path", type=str, help="Path to the CI configuration YAML"
+    )
+    parser.add_argument(
+        "--repo-root", type=str, default="", help="Path to the repository root"
     )
     args = parser.parse_args()
 
@@ -173,7 +309,10 @@ def main():
             f"ghcr-cve-scan={json.dumps(ci_config.ghcr.upload, indent=indent)}\n"  # pylint: disable=no-member
         )
         gh_out.write(
-            f"build-matrix={json.dumps(ci_config.build_matrix(), indent=indent)}\n"
+            f"build-matrix={json.dumps(ci_config.build_matrix(args.repo_root), indent=indent)}\n"
+        )
+        gh_out.write(
+            f"upload-matrix={json.dumps(ci_config.upload_matrix(args.repo_root), indent=indent)}\n"
         )
 
 
